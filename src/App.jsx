8fc9db999dfbb911e70e173d80f5579e82705734
Scarlett Valencia Pulido <svalenciapul2021@fau.edu>
2025-04-26 18:13:50 -0400

    Added React Router and created blank files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React from 'react';
+import { useRoutes } from 'react-router-dom'
+import Home from './pages/Home'
+import CreatePost from './pages/CreatePost'
+import EditPost from './pages/EditPost'
+import { Link } from 'react-router-dom'
+import PostDetails from './pages/PostDetails'; 
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+
+const App = () => {
+
+  // const backgroundStyle = {
+  //   backgroundImage: "url('/images/bluee.png')",
+  //   backgroundSize: 'cover',
+  //   backgroundPosition: 'center',
+  // };
+
+  // Sets up routes
+  let element = useRoutes([
+    {
+      path: "/",
+      element:<Home />
+    },
+    {
+      path:"/edit/:id",
+      element: <EditPost />
+    },
+    {
+      path:"/new",
+      element: <CreatePost />
+    },
+    {
+      path: '/post/:id',  
+      element: <PostDetails />
+    }
+  ]);
+
+  return ( 
+
+
+    <div className="App" style={backgroundStyle}>
+      <div className="header-buttons">
+          <Link to="/"><button className="headerBtn">Explore Toons</button></Link>
+          <Link to="/new"><button className="headerBtn">Create Toon</button></Link>
+        </div>
+        
+      <div className="header">
+        <div className="header-title">
+          <h1>Toon Squad</h1>
+          <h2>A Scarlett Valencia Production</h2>
+        </div>
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {element}
+    </div>
+
+
+
+  );
 }
 
-export default App
+export default App;
