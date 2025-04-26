eabca77e04c5f7cce1ab0e6da266ab68f50f1376
Scarlett Valencia Pulido <svalenciapul2021@fau.edu>
2025-04-26 18:14:49 -0400

    Created SelectInput.js

---
+++ src/components/SelectInput.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SelectInput = ({ label, name, value, options, onChange }) => {
+  return (
+    <div className="select-input">
+      <label htmlFor={name}>{label}:</label>
+      <select id={name} name={name} value={value} onChange={onChange}>
+        {options.map((opt) => (
+          <option key={opt.value} value={opt.value}>
+            {opt.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default SelectInput;
